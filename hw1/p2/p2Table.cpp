1b5faa8a96c8afc9d0f2214909c2d36b9ed2a26b
AdrianHsu <adrianhsu1995@gmail.com>
2016-08-29 01:34:54 +0800

    getline testing

--- hw1/p2/p2Table.cpp
+++ hw1/p2/p2Table.cpp
@@ -7,5 +7,53 @@ using namespace std;
 bool
 Table::read(const string& csvFile)
 {
+  fstream fs;
+  fs.open(csvFile, ios::in);
+  
+  if(!fs) {
+   return false; 
+  }
+
+  int i = 0;
+  
+  string str;
+  int num = 0;
+
+  while( getline(fs, str, '\r') ) {
+    
+    if(str == "")
+      break;
+
+    vector<int> vec;
+    cout << "str=" << str << endl;
+    stringstream ss(str);
+    
+    while( getline(ss, str, ',') ) {
+
+      if(str == "") {
+        num = -100;
+      } else {
+        num = atoi(str.c_str());
+      }
+      cout << num << endl;
+    }
+    if(str == "") {
+      num = -100;
+      cout << num << endl;
+    }
+      
+    // Row row(vec.size());
+    // _rows.push_back(row);
+    
+    // for(int j = 0; j < vec.size(); j++) {
+    //  _rows[i][j] = vec[j];
+      //cout << "_rows " << i << "," << j << " = " << _rows[i][j] << endl;  
+    // }
+    i++;
+  }
+ 
+  cout << "done!" << endl;
+  
   return true; // TODO
 }
+
--- hw1/p2/p2Table.h
+++ hw1/p2/p2Table.h
@@ -5,17 +5,22 @@
 #include <string>
 
 #include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
 
 using namespace std;
 
 class Row
 {
 public:
-   const int operator[] (size_t i) const { return _data[i]; } // TODO
-   int& operator[] (size_t i) { return _data[i]; } // TODO
+  const int operator[] (size_t i) const { return _data[i]; } // TODO
+  int& operator[] (size_t i) { return _data[i]; } // TODO
+  Row(int s): NUM_COL(0) { _data = new int[s]; NUM_COL = s;} 
 
 private:
    int  *_data;
+   int NUM_COL;
 };
 
 class Table 
