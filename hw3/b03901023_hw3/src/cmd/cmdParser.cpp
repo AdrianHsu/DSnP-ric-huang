f954c09846ff8755d67ef6c0f9ea15e664f45399
AdrianHsu <adrianhsu1995@gmail.com>
2016-10-09 13:39:42 +0800

    done compressing

---
+++ hw3/b03901023_hw3/src/cmd/cmdParser.cpp
@@ -0,0 +1,457 @@
+/****************************************************************************
+  FileName     [ cmdParser.cpp ]
+  PackageName  [ cmd ]
+  Synopsis     [ Define command parsing member functions for class CmdParser ]
+  Author       [ Chung-Yang (Ric) Huang ]
+  Copyright    [ Copyleft(c) 2007-present LaDs(III), GIEE, NTU, Taiwan ]
+****************************************************************************/
+#include <cassert>
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include "util.h"
+#include "cmdParser.h"
+
+using namespace std;
+
+//----------------------------------------------------------------------
+//    External funcitons
+//----------------------------------------------------------------------
+void mybeep();
+
+
+//----------------------------------------------------------------------
+//    Member Function for class cmdParser
+//----------------------------------------------------------------------
+// return false if file cannot be opened
+// Please refer to the comments in "DofileCmd::exec", cmdCommon.cpp
+bool
+CmdParser::openDofile(const string& dof)
+{
+   // TODO...
+   //_dofile is "dofiles/do1"
+   if(_dofileStack.size() >= 1024) return false;
+
+   _dofile = new ifstream(dof.c_str());
+   
+   if(!_dofile->is_open()) {
+     
+      delete _dofile;
+      if(_dofileStack.size() != 0)
+         _dofile = _dofileStack.top();
+      else {
+         _dofile = 0;
+      }
+      return false;
+   }
+
+   _dofileStack.push(_dofile);
+   return true;
+
+}
+
+// Must make sure _dofile != 0
+void
+CmdParser::closeDofile()
+{
+   assert(_dofile != 0);
+   // TODO...
+   //if(_dofileStack.size() == 0)
+   //   return;
+
+   _dofileStack.pop();
+   delete _dofile;
+
+   if(_dofileStack.size() == 0)
+      _dofile = 0;
+   else
+      _dofile = _dofileStack.top();
+}
+
+// Return false if registration fails
+bool
+CmdParser::regCmd(const string& cmd, unsigned nCmp, CmdExec* e)
+{
+   // Make sure cmd hasn't been registered and won't cause ambiguity
+   string str = cmd;
+   unsigned s = str.size();
+   if (s < nCmp) return false;
+   while (true) {
+      if (getCmd(str)) return false;
+      if (s == nCmp) break;
+      str.resize(--s);
+   }
+
+   // Change the first nCmp characters to upper case to facilitate
+   //    case-insensitive comparison later.
+   // The strings stored in _cmdMap are all upper case
+   //
+   assert(str.size() == nCmp);  // str is now mandCmd
+   string& mandCmd = str;
+   for (unsigned i = 0; i < nCmp; ++i)
+      mandCmd[i] = toupper(mandCmd[i]);
+   string optCmd = cmd.substr(nCmp);
+   assert(e != 0);
+   e->setOptCmd(optCmd);
+
+   // insert (mandCmd, e) to _cmdMap; return false if insertion fails.
+   return (_cmdMap.insert(CmdRegPair(mandCmd, e))).second;
+}
+
+// Return false on "quit" or if excetion happens
+CmdExecStatus
+CmdParser::execOneCmd()
+{
+   bool newCmd = false;
+   if (_dofile != 0)
+      newCmd = readCmd(*_dofile);
+   else
+      newCmd = readCmd(cin);
+
+   // execute the command
+   if (newCmd) {
+      string option;
+      CmdExec* e = parseCmd(option);
+      if (e != 0)
+         return e->exec(option);
+   }
+   return CMD_EXEC_NOP;
+}
+
+// For each CmdExec* in _cmdMap, call its "help()" to print out the help msg.
+// Print an endl at the end.
+void
+CmdParser::printHelps() const
+{
+   // TODO...
+   CmdMap::const_iterator it;
+   for(it = _cmdMap.begin(); it != _cmdMap.end(); it++){
+      it->second->help();
+   }
+   cout << endl;
+}
+
+void
+CmdParser::printHistory(int nPrint) const
+{
+   assert(_tempCmdStored == false);
+   if (_history.empty()) {
+      cout << "Empty command history!!" << endl;
+      return;
+   }
+   int s = _history.size();
+   if ((nPrint < 0) || (nPrint > s))
+      nPrint = s;
+   for (int i = s - nPrint; i < s; ++i)
+      cout << "   " << i << ": " << _history[i] << endl;
+}
+
+//
+// Parse the command from _history.back();
+// Let string str = _history.back();
+//
+// 1. Read the command string (may contain multiple words) from the leading
+//    part of str (i.e. the first word) and retrive the corresponding
+//    CmdExec* from _cmdMap
+//    ==> If command not found, print to cerr the following message:
+//        Illegal command!! "(string cmdName)"
+//    ==> return it at the end.
+// 2. Call getCmd(cmd) to retrieve command from _cmdMap.
+//    "cmd" is the first word of "str".
+// 3. Get the command options from the trailing part of str (i.e. second
+//    words and beyond) and store them in "option"
+//
+CmdExec*
+CmdParser::parseCmd(string& option)
+{
+   assert(_tempCmdStored == false);
+   assert(!_history.empty());
+   string str = _history.back();
+
+   // TODO...
+   assert(str[0] != 0 && str[0] != ' ');
+   //str == current command + options
+   string cmd = "";
+   myStrGetTok(str, cmd);
+
+   // str token is now "HELpp"
+   CmdExec* e;
+   e = getCmd(cmd);
+   if(e == NULL) {
+      cerr << "Illegal command!! (" << cmd << ")";
+      return e;
+   }
+   
+   size_t i;
+   for(i = cmd.size(); i < str.size(); i++) {
+      if(str[i] != ' ')   break;
+   }
+
+   option = str.substr(i);
+
+   return e;
+}
+
+// This function is called by pressing 'Tab'.
+// It is to list the partially matched commands.
+// "str" is the partial string before current cursor position. It can be 
+// a null string, or begin with ' '. The beginning ' ' will be ignored.
+//
+// Several possibilities after pressing 'Tab'
+// (Let $ be the cursor position)
+// 1. [Before] Null cmd
+//    cmd> $
+//    -----------
+//    [Before] Cmd with ' ' only
+//    cmd>     $
+//    [After Tab]
+//    ==> List all the commands, each command is printed out by:
+//           cout << setw(12) << left << cmd;
+//    ==> Print a new line for every 5 commands
+//    ==> After printing, re-print the prompt and place the cursor back to
+//        original location (including ' ')
+//
+// 2. [Before] partially matched (multiple matches)
+//    cmd> h$                   // partially matched
+//    [After Tab]
+//    HELp        HIStory       // List all the parially matched commands
+//    cmd> h$                   // and then re-print the partial command
+//    -----------
+//    [Before] partially matched (multiple matches)
+//    cmd> h$aaa                // partially matched with trailing characters
+//    [After Tab]
+//    HELp        HIStory       // List all the parially matched commands
+//    cmd> h$aaa                // and then re-print the partial command
+//
+// 3. [Before] partially matched (single match)
+//    cmd> he$
+//    [After Tab]
+//    cmd> heLp $
+//    -----------
+//    [Before] partially matched with trailing characters (single match)
+//    cmd> he$hahah
+//    [After Tab]
+//    cmd> heLp $hahaha
+//    ==> Automatically complete on the same line
+//    ==> The auto-expanded part follow the strings stored in cmd map and
+//        cmd->_optCmd. Insert a space after "heLp"
+//
+// 4. [Before] No match
+//    cmd> hek$
+//    [After Tab]
+//    ==> Beep and stay in the same location
+//
+// 5. [Before] Already matched
+//    cmd> help asd$fg
+//    [After] Print out the usage for the already matched command
+//    Usage: HELp [(string cmd)]
+//    cmd> help asd$fg
+//
+// 6. [Before] Cursor NOT on the first word and NOT matched command
+//    cmd> he haha$kk
+//    [After Tab]
+//    ==> Beep and stay in the same location
+//
+void
+CmdParser::listCmd(const string& str)
+{
+   // TODO...
+   bool _case_one = false;
+   char* _myPtr = _readBuf;
+   string s = "";
+   size_t _space = 0;
+   if(_myPtr == _readBufPtr) {
+      _case_one = true;
+   }else {
+      while( *_myPtr == ' ') {
+         _myPtr++;
+         _space++;
+         if(_myPtr == _readBufPtr) {
+            _case_one = true;
+            break;
+         }
+      }
+   }
+   if(_case_one) {
+      
+      cout << endl;
+      size_t count = 0;
+      for(CmdMap::const_iterator it = _cmdMap.begin(); it != _cmdMap.end(); it++){
+         cout << setw(12) << left << it->first + it->second->getOptCmd();
+         count++;
+         if (count == 5){
+            count = 0;
+            cout << endl;
+         }
+      }
+      reprintCmd();
+      return;
+   }
+   int len = _readBufPtr - _myPtr;
+   // *_myPtr is now the first char with no space before it
+   char* _myReadBuf = _myPtr;
+   char* _mySpacePtr = _myReadBuf; //token last char occured to space
+   // s is now str with no space before, up to _readBufPtr position
+   s = str.substr(_space, _space + len);
+   string token = "";
+   myStrGetTok(s, token);
+
+   while(*_mySpacePtr != ' ') {
+      _mySpacePtr++;
+      if(_mySpacePtr == _readBufPtr){ 
+         break;
+      }
+   }
+   string tok = "";
+   while(_myPtr != _mySpacePtr) {
+      tok += *_myPtr;
+      _myPtr++;
+   }
+   if(_mySpacePtr != _readBufPtr) {
+      for(CmdMap::const_iterator it = _cmdMap.begin(); it != _cmdMap.end(); it++) {
+         string _cur_cmd = it->first + it->second->getOptCmd();
+         if(myStrNCmp(_cur_cmd, tok, it->first.size()) == 0){
+            cout << endl;
+            getCmd(_cur_cmd)->usage(cout);
+            reprintCmd();
+            return;
+         }
+      }
+      mybeep();
+      return;
+   }
+   else { 
+      vector<string> multi_vec;
+      for(CmdMap::const_iterator it = _cmdMap.begin() ; it != _cmdMap.end(); it++) {
+         string _cur_cmd = it->first + it->second->getOptCmd();
+         if(_cur_cmd.size() < token.size()) continue;
+         if((myStrNCmp(_cur_cmd, token, token.size()) == 0)) {
+            multi_vec.push_back(_cur_cmd);
+         }
+      }
+      if(multi_vec.size() == 0) {
+         mybeep();
+         return;
+      } else if(multi_vec.size() == 1) {
+         for (size_t i = token.size(); i < multi_vec[0].size(); ++i) {
+            insertChar(multi_vec[0][i]);
+         } insertChar(' ');
+      } else {
+         cout << endl;
+         for(size_t i = 0; i < multi_vec.size(); i++){
+            cout << setw(12) << left << multi_vec[i];
+            if (i == 5){
+               cout << endl;
+            }
+         }
+         reprintCmd();
+         return;
+      }
+   }
+}
+
+// cmd is a copy of the original input
+//
+// return the corresponding CmdExec* if "cmd" matches any command in _cmdMap
+// return 0 if not found.
+//
+// Please note:
+// ------------
+// 1. The mandatory part of the command string (stored in _cmdMap) must match
+// 2. The optional part can be partially omitted.
+// 3. All string comparison are "case-insensitive".
+//
+CmdExec*
+CmdParser::getCmd(string cmd)
+{
+   CmdExec* e = 0;
+   // TODO...
+   CmdMap::const_iterator it;
+   for(it = _cmdMap.begin(); it != _cmdMap.end(); it++) {
+      string key = it->first;
+      string optcmd = (it->second)->getOptCmd();
+      if(myStrNCmp( key+optcmd , cmd, key.size() ) == 0) {
+         e = it->second;
+         break;
+      }
+   }
+   return e;
+}
+
+
+//----------------------------------------------------------------------
+//    Member Function for class CmdExec
+//----------------------------------------------------------------------
+// Return false if error options found
+// "optional" = true if the option is optional XD
+// "optional": default = true
+//
+bool
+CmdExec::lexSingleOption
+(const string& option, string& token, bool optional) const
+{
+   size_t n = myStrGetTok(option, token);
+   if (!optional) {
+      if (token.size() == 0) {
+         errorOption(CMD_OPT_MISSING, "");
+         return false;
+      }
+   }
+   if (n != string::npos) {
+      errorOption(CMD_OPT_EXTRA, option.substr(n));
+      return false;
+   }
+   return true;
+}
+
+// if nOpts is specified (!= 0), the number of tokens must be exactly = nOpts
+// Otherwise, return false.
+//
+bool
+CmdExec::lexOptions
+(const string& option, vector<string>& tokens, size_t nOpts) const
+{
+   string token;
+   size_t n = myStrGetTok(option, token);
+   while (token.size()) {
+      tokens.push_back(token);
+      n = myStrGetTok(option, token, n);
+   }
+   if (nOpts != 0) {
+      if (tokens.size() < nOpts) {
+         errorOption(CMD_OPT_MISSING, "");
+         return false;
+      }
+      if (tokens.size() > nOpts) {
+         errorOption(CMD_OPT_EXTRA, tokens[nOpts]);
+         return false;
+      }
+   }
+   return true;
+}
+
+CmdExecStatus
+CmdExec::errorOption(CmdOptionError err, const string& opt) const
+{
+   switch (err) {
+      case CMD_OPT_MISSING:
+         cerr << "Error: Missing option";
+         if (opt.size()) cerr << " after (" << opt << ")";
+         cerr << "!!" << endl;
+      break;
+      case CMD_OPT_EXTRA:
+         cerr << "Error: Extra option!! (" << opt << ")" << endl;
+      break;
+      case CMD_OPT_ILLEGAL:
+         cerr << "Error: Illegal option!! (" << opt << ")" << endl;
+      break;
+      case CMD_OPT_FOPEN_FAIL:
+         cerr << "Error: cannot open file \"" << opt << "\"!!" << endl;
+      break;
+      default:
+         cerr << "Error: Unknown option error type!! (" << err << ")" << endl;
+      exit(-1);
+   }
+   return CMD_EXEC_ERROR;
+}
+
---
+++ hw3/b03901023_hw3/src/db/dbTable.cpp
@@ -0,0 +1,303 @@
+/****************************************************************************
+  FileName     [ dbTable.cpp ]
+  PackageName  [ db ]
+  Synopsis     [ Define database Table member functions ]
+  Author       [ Chung-Yang (Ric) Huang ]
+  Copyright    [ Copyleft(c) 2015-present LaDs(III), GIEE, NTU, Taiwan ]
+****************************************************************************/
+
+#include <iomanip>
+#include <string>
+#include <cctype>
+#include <cassert>
+#include <set>
+#include <algorithm>
+#include "dbTable.h"
+
+using namespace std;
+
+/*****************************************/
+/*          Global Functions             */
+/*****************************************/
+ostream& operator << (ostream& os, const DBRow& r)
+{
+   // TODO: to print out a row.
+   // - Data are seperated by a space. No trailing space at the end.
+   // - Null cells are printed as '.'
+   size_t i = 0, size = r.size();
+   for( ; i < size; i++ ){
+      DBTable::printData(os, r[i]);
+      if(i != size - 1) os << ' ';
+   }
+   os << endl;
+   return os;
+}
+
+ostream& operator << (ostream& os, const DBTable& t)
+{
+   // TODO: to print out a table
+   // - Data are seperated by setw(6) and aligned right.
+   // - Null cells are printed as '.'
+   for(size_t i = 0; i < t.nRows(); i++) { 
+      for(size_t j = 0; j < t.nCols(); j++) {
+         if(t[i][j] == INT_MAX)
+            os << setw(6) << right << ".";
+         else
+            os << setw(6) << right << t[i][j];
+      }
+      os << endl;
+   }
+   os << endl;
+   return os;
+}
+
+ifstream& operator >> (ifstream& ifs, DBTable& t)
+{
+
+   string str;
+   int num = 0;
+
+   while( getline(ifs, str, '\r') ) {
+
+      if(str == "\n")
+         break;
+      else if(str == "") { // prevent case: 3^M^M
+         // however, problem will occured when "^M at the end"
+         
+         DBRow row = DBRow();
+         row.addData(INT_MAX);
+         t.addRow(row);
+         
+         continue;
+
+      }
+      // you don’t need to handle syntax errors in reading .csv file 
+      // (e.g. missing ^M at the end) or parsing commands.
+
+      vector<int> vec;
+      stringstream ss(str);
+
+      while( getline(ss, str, ',') ) {
+
+         if(str == "") { //prevent case: ^M,3^M
+            num = INT_MAX;
+         } else {
+            num = atoi(str.c_str());
+         }
+         vec.push_back(num);
+      }
+      // prevent case: 3,^M ...etc
+      if(str == "") {
+         num = INT_MAX;
+         vec.push_back(num);
+      }
+
+      DBRow row(vec);
+      t.addRow(row);
+   }
+   
+   if(t.nRows() != 0)
+      t.delRow(t.nRows() - 1); // solve "^M at the end", 
+   // since "missing ^M at the end" special case will not happened
+
+   return ifs;
+}
+
+/*****************************************/
+/*   Member Functions for class DBRow    */
+/*****************************************/
+void
+DBRow::removeCell(size_t c)
+{
+   // TODO
+   if(c < size())
+      _data.erase(_data.begin() + c); 
+   else
+      cerr << "error2" << endl;
+      
+}
+
+/*****************************************/
+/*   Member Functions for struct DBSort  */
+/*****************************************/
+bool
+DBSort::operator() (const DBRow& r1, const DBRow& r2) const
+{
+   // TODO: called as a functional object that compares the data in r1 and r2
+   //       based on the order defined in _sortOrder
+   for(size_t i = 0; i < _sortOrder.size(); i++){
+      int n = _sortOrder[i];
+      if(r1[n] > r2[n]) return false;
+      if(r1[n] < r2[n]) return true;
+   }
+   return false;
+}
+
+/*****************************************/
+/*   Member Functions for class DBTable  */
+/*****************************************/
+void
+DBTable::reset()
+{
+   // TODO
+   vector<DBRow> tmp;
+   tmp.swap(_table);
+}
+
+void
+DBTable::addCol(const vector<int>& d)
+{
+   // TODO: add a column to the right of the table. Data are in 'd'.
+   for(size_t i = 0; i < d.size(); i++) {
+      
+      if(d.size() != nRows()) {
+         cerr << "error3" << endl;
+      }
+      _table[i].addData(d[i]);
+   }
+}
+
+void
+DBTable::delRow(int c)
+{
+   // TODO: delete row #c. Note #0 is the first row.
+   if(!_table[c].empty())
+      _table.erase(_table.begin() + c); 
+   else
+      cerr << "error4" << endl;
+}
+
+void
+DBTable::delCol(int c)
+{
+   // delete col #c. Note #0 is the first row.
+   for (size_t i = 0, n = _table.size(); i < n; ++i)
+      _table[i].removeCell(c);
+}
+
+// For the following getXXX() functions...  (except for getCount())
+// - Ignore null cells
+// - If all the cells in column #c are null, return NAN
+// - Return "float" because NAN is a float.
+float
+DBTable::getMax(size_t c) const
+{
+   // TODO: get the max data in column #c
+   bool flag = false;
+   int max = 0;
+   int i = 0, n = nRows();
+   for( ; i < n; i++ ){
+      int val = _table[i][c];
+      if((!flag && val != INT_MAX) || (val > max && val != INT_MAX)){
+         max = val;
+         flag = true;
+      }
+   }
+   if(flag) return (float)max;
+   return NAN;
+
+}
+
+float
+DBTable::getMin(size_t c) const
+{
+   // TODO: get the min data in column #c
+   bool flag = false;
+   int min = 0;
+   int i = 0, n = nRows();
+   for( ; i < n; i++ ){
+      int val = _table[i][c];
+      if((!flag && val != INT_MAX) || (val < min && val != INT_MAX)){
+         min = val;
+         flag = true;
+      }
+   }
+   if(flag) return (float)min;
+   return NAN;
+}
+
+float 
+DBTable::getSum(size_t c) const
+{
+   // TODO: compute the sum of data in column #c
+   int sum = 0, i = 0, n = nRows();
+   bool flag = false;
+   for( ; i < n; i++ ){
+      int val = _table[i][c];
+      if(val != INT_MAX) {
+         sum += val;
+         flag = true;
+      }
+   }
+   if (flag)   return sum;
+   return NAN;
+}
+
+int
+DBTable::getCount(size_t c) const
+{
+   // TODO: compute the number of distinct data in column #c
+   // - Ignore null cells
+
+
+   set<int> _set;
+   int i = 0, n = nRows();
+   for( ; i < n; i++ ){
+      
+      if(_table[i][c] != INT_MAX){
+         _set.insert(_table[i][c]);
+      }
+   }  
+   
+   return _set.size();
+}
+
+float
+DBTable::getAve(size_t c) const
+{
+   // TODO: compute the average of data in column #c
+   int sum = 0, count = 0, i = 0, n = nRows();
+   for( ; i < n; i++ ){
+      int val = _table[i][c];
+      if(val != INT_MAX){
+         sum += val;
+         count ++;
+      }
+   }
+   if (count == 0)   return NAN;
+   return 1.0 * sum / count;
+}
+
+void
+DBTable::sort(const struct DBSort& s)
+{
+   // TODO: sort the data according to the order of columns in 's'
+   std::sort(_table.begin(), _table.end(), s);
+
+}
+
+void
+DBTable::printCol(size_t c) const
+{
+   // TODO: to print out a column.
+   // - Data are seperated by a space. No trailing space at the end.
+   // - Null cells are printed as '.'
+   int i = 0, size = nRows();
+   for( ; i < size; i++ ){
+      printData(cout, _table[i][c]);
+      if(i != size - 1) cout<< ' ';
+   }
+   cout << endl;
+}
+
+void
+DBTable::printSummary() const
+{
+   size_t nr = nRows(), nc = nCols(), nv = 0;
+   for (size_t i = 0; i < nr; ++i)
+      for (size_t j = 0; j < nc; ++j)
+         if (_table[i][j] != INT_MAX) ++nv;
+   cout << "(#rows, #cols, #data) = (" << nr << ", " << nc << ", "
+        << nv << ")" << endl;
+}
+
